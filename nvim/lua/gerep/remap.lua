8253ae6198ec0686f201af65c838f0447bbee3a3
Gerep <daniel.gerep@skiff.com>
2024-01-27 06:31:38 -0300

    Update remap.lua

--- nvim/lua/gerep/remap.lua
+++ nvim/lua/gerep/remap.lua
@@ -26,14 +26,11 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
---vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<C-f>", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>S", [[:%s/<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-
-vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, {buffer = 0})
+vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { buffer = 0 })

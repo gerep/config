5f2280abc21e6d3b1f93a230948a6c2e51cc1df9
Gerep <daniel.gerep@skiff.com>
2024-01-27 06:32:24 -0300

    Update golang.lua

--- nvim/lua/gerep/golang.lua
+++ nvim/lua/gerep/golang.lua
@@ -1,17 +1,17 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-    local bufnr = vim.api.nvim_get_current_buf()
-    local params = vim.lsp.util.make_range_params(nil, nil, bufnr)
-    params.context = {only = {"source.organizeImports"}}
-    local result = vim.lsp.buf_request_sync(bufnr, "textDocument/codeAction", params)
-    for cid, res in pairs(result or {}) do
-      for _, r in pairs(res.result or {}) do
-        if r.edit then
-          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+    pattern = "*.go",
+    callback = function()
+        local bufnr = vim.api.nvim_get_current_buf()
+        local params = vim.lsp.util.make_range_params(nil, nil, bufnr)
+        params.context = { only = { "source.organizeImports" } }
+        local result = vim.lsp.buf_request_sync(bufnr, "textDocument/codeAction", params)
+        for cid, res in pairs(result or {}) do
+            for _, r in pairs(res.result or {}) do
+                if r.edit then
+                    local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                    vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                end
+            end
         end
-      end
     end
-  end
 })

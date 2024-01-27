81ba0f576cca2d578b84e73d6120b6e3573ddf8d
Gerep <daniel.gerep@skiff.com>
2024-01-27 06:30:54 -0300

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,26 +3,27 @@ package.path = config_path .. '/?.lua;' .. config_path .. '/?/init.lua;' .. pack
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("gerep")
 require("lazy").setup({
-  require("after.plugins.telescope"),
-  require("after.plugins.colour"),
-  require("after.plugins.treesitter"),
-  require("after.plugins.harpoon"),
-  require("after.plugins.undotree"),
-  require("after.plugins.lsp"),
-  require("after.plugins.gitsigns"),
-  "nvim-treesitter/playground",
+    require("after.plugins.telescope"),
+    require("after.plugins.colour"),
+    require("after.plugins.treesitter"),
+    require("after.plugins.harpoon"),
+    require("after.plugins.undotree"),
+    require("after.plugins.lsp"),
+    require("after.plugins.gitsigns"),
+    "nvim-treesitter/playground",
+    "tpope/vim-surround",
 })

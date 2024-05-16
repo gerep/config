adb817063d60b22aa5fead12ac22748d0be9f0ff
danielgerep-bnet <daniel.gerep@contractor.zenimax.com>
2024-05-16 16:43:44 -0300

    asdasd

--- nvim/lua/plugins/colour.lua
+++ nvim/lua/plugins/colour.lua
@@ -1,9 +1,16 @@
 return {
   {
-    "sainnhe/everforest",
+    --"sainnhe/everforest",
+    --"davidosomething/vim-colors-meh",
+    --"vonheikemen/rubber-themes.vim",
+    --"dikiaap/minimalist",
+    --"Marfisc/vorange",
+    --"kar9222/minimalist.nvim",
+    "rebelot/kanagawa.nvim",
     config = function()
       vim.cmd [[ set background=dark ]]
-      vim.cmd [[ colorscheme everforest ]]
+      --vim.cmd [[ colorscheme everforest ]]
+      vim.cmd [[ colorscheme kanagawa-wave ]]
     end
   }
 }

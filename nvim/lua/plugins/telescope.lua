e1c21852c52a4daad61961fb719748366057bd73
Gerep <daniel.gerep@gmail.com>
2024-05-16 16:45:32 -0300

    Update telescope.lua

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
     require('telescope').setup {
       defaults = {
         file_ignore_patterns = { "vendor/", "node_modules/" },
-        path_display = {"truncate"},
+        path_display = { "truncate" },
       },
     }
 
@@ -16,11 +16,15 @@ return {
     vim.keymap.set("n", "<leader>gg", telescope_builtin.grep_string, {})
     vim.keymap.set("n", "<leader>b", telescope_builtin.buffers, {})
     vim.keymap.set("n", "<leader>s", telescope_builtin.lsp_document_symbols, {})
-    vim.keymap.set("n", "<leader>d", function()
+    vim.keymap.set("n", "<leader>gs", telescope_builtin.git_status, {})
+    vim.keymap.set("n", "<leader>dd", function()
       telescope_builtin.diagnostics({ bufnr = 0 })
     end)
-    vim.keymap.set("n", "<leader>D", function()
+    vim.keymap.set("n", "<leader>DD", function()
       telescope_builtin.diagnostics()
+    end)
+    vim.keymap.set("n", "<leader>df", function()
+      vim.diagnostic.open_float({ buffer = 0 })
     end)
     vim.keymap.set("n", "gr", telescope_builtin.lsp_references, {})
     vim.keymap.set("n", "gi", telescope_builtin.lsp_implementations, {})

ede741328d001587d928e58cb549fdfbf8ce6e2e
Gerep <daniel.gerep@gmail.com>
2024-05-16 16:45:48 -0300

    Update treesitter_playground.lua

--- nvim/lua/plugins/treesitter_playground.lua
+++ nvim/lua/plugins/treesitter_playground.lua
@@ -4,5 +4,10 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
+    config = function()
+      require 'treesitter-context'.setup {
+        max_lines = 1,
+      }
+    end
   }
 }

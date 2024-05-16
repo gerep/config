4ad767a9f0c0565ccfdcae0b611c59c0ec1bb624
Gerep <daniel.gerep@gmail.com>
2024-05-16 16:45:17 -0300

    Update colour.lua

--- nvim/lua/plugins/colour.lua
+++ nvim/lua/plugins/colour.lua
@@ -1,9 +1,10 @@
 return {
   {
-    "sainnhe/everforest",
+    "rebelot/kanagawa.nvim",
     config = function()
       vim.cmd [[ set background=dark ]]
-      vim.cmd [[ colorscheme everforest ]]
+      --vim.cmd [[ colorscheme everforest ]]
+      vim.cmd [[ colorscheme kanagawa-wave ]]
     end
   }
 }

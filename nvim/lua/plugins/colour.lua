48bc62cc11165f70c74424168660af5751733974
gerep <181501+gerep@users.noreply.github.com>
2024-10-10 12:48:45 -0300

    trying rosepine

--- nvim/lua/plugins/colour.lua
+++ nvim/lua/plugins/colour.lua
@@ -1,10 +1,11 @@
 return {
   {
     -- "neanias/everforest-nvim",
-    "sainnhe/sonokai",
+    "rose-pine/neovim",
+    -- "sainnhe/sonokai",
     config = function()
       -- vim.cmd [[ colorscheme everforest ]]
-      vim.cmd [[ colorscheme sonokai ]]
+      vim.cmd [[ colorscheme rose-pine ]]
     end
   }
 }

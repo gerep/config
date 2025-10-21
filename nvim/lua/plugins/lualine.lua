9bb4d0c56951c46db6e3f696417c4ea8d0725a63
gerep <181501+gerep@users.noreply.github.com>
2025-10-21 16:59:03 -0300

    lualine show entire file path

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -4,5 +4,13 @@ return {
 	opts = {
 		theme = "auto",
 		icons_enabled = true,
+		sections = {
+			lualine_c = {
+				{
+					"filename",
+					path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+				},
+			},
+		},
 	},
 }

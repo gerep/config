6f115f43fdc34595c9f9864a4c7be0450c175357
gerep <181501+gerep@users.noreply.github.com>
2025-12-17 21:51:30 -0300

    theme change

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,8 +1,8 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme("catppuccin-frappe")
+        vim.cmd.colorscheme("kanagawa")
     end,
 }
